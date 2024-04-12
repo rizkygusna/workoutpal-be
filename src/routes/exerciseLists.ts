42baae6644e73ce468db2dcef03892ce3b9fd05f
Rizki Alfarid <gusnafarid@gmail.com>
2024-04-12 22:41:01 +0700

    add edit exercise list endpoint

--- src/routes/exerciseLists.ts
+++ src/routes/exerciseLists.ts
@@ -4,6 +4,8 @@ import { verifyToken } from "./auth";
 
 const router = express.Router();
 
+// TODO: add get exercise list by id endpoint
+
 router.get("/", verifyToken, async (req, res) => {
   const { userId } = req.query;
   console.log("Received id: ", userId);
@@ -29,10 +31,30 @@ router.post("/", verifyToken, async (req, res) => {
     );
     if (result.rowsAffected <= 0)
       return res.status(404).json("Exercise list not found");
+    // get the affected row id then return the row with id
     res.json({ name: listName, description: description });
   } catch (error) {
     console.log(error);
     return res.status(500).json("Error adding exercise list.");
+  }
+});
+
+router.put("/:listId", verifyToken, async (req, res) => {
+  const listId = req.params.listId;
+  const { listName, description } = req.body;
+
+  try {
+    const result = await client.execute({
+      sql: "UPDATE exercise_list SET list_name = ?, description = ? WHERE list_id = ?",
+      args: [listName, description, listId],
+    });
+    if (result.rowsAffected <= 0)
+      return res.status(404).json("Exercise list not found");
+    // get the affected row id then return the row with id
+    res.json({ name: listName, description: description });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Error edit exercise list.");
   }
 });
 
