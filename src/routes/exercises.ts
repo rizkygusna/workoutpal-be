f154c6b50464fec5b2144cd01ce6df798bbb181c
Rizki Alfarid <gusnafarid@gmail.com>
2024-05-07 14:24:26 +0700

    add endpoint for edit & get exercise by id

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -9,7 +9,7 @@ router.get("/", verifyToken, async (req, res) => {
   try {
     if (!userId) return res.status(401).json("userId is required.");
     const result = await client.execute(
-      `SELECT * FROM exercise WHERE user_id IS NULL OR user_id = ${userId}`
+      `SELECT * FROM exercise WHERE user_id IS NULL OR user_id = '${userId}'`
     );
     const rowsWithoutUserIdColumn = result.rows.map((row) => {
       return {
@@ -22,6 +22,63 @@ router.get("/", verifyToken, async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json("Error fetching exercise list.");
+  }
+});
+
+router.put("/:exerciseId", verifyToken, async (req, res) => {
+  const exerciseId = req.params.exerciseId;
+  const { name, description } = req.body;
+  try {
+    const result = await client.execute({
+      sql: "UPDATE exercise SET exercise_name = ?, exercise_description = ? WHERE exercise_id = ?",
+      args: [name, description, exerciseId],
+    });
+    if (result.rowsAffected <= 0)
+      return res.status(404).json("Exercise not found");
+    res.status(200).json({
+      id: exerciseId,
+      name: name,
+      description: description,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Error edit exercise.");
+  }
+});
+
+router.get("/:exerciseId", verifyToken, async (req, res) => {
+  const exerciseId = req.params.exerciseId;
+  try {
+    const result = await client.execute(
+      `SELECT * FROM exercise WHERE exercise_id = '${exerciseId}'`
+    );
+    if (result.rows.length <= 0) res.status(404).json("Exercise not found");
+    const rowsWithoutUserIdColumn = result.rows.map((row) => {
+      return {
+        id: row["exercise_id"],
+        name: row["exercise_name"],
+        description: row["exercise_description"],
+      };
+    });
+    res.status(200).json(rowsWithoutUserIdColumn[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Error fetching exercise.");
+  }
+});
+
+router.delete("/:exerciseId", verifyToken, async (req, res) => {
+  const exerciseId = req.params.exerciseId;
+  try {
+    const result = await client.execute(
+      `DELETE FROM exercise WHERE exercise_id = ${exerciseId}`
+    );
+    if (result.rowsAffected <= 0)
+      return res.status(404).json("Exercise not found.");
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Error delete exercise list");
   }
 });
 
