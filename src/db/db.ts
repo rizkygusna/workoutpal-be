a48b7fb04bbd534d913224240f03c0f463af17d7
Rizki Alfarid <gusnafarid@gmail.com>
2024-05-21 22:21:22 +0700

    undo embedded replica config

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 import 'dotenv/config';
 
 export const client = createClient({
-  url: process.env.DATABASE_REPLICA!,
-  syncUrl: process.env.DATABASE_URL,
+  // url: process.env.DATABASE_REPLICA!,
+  url: process.env.DATABASE_URL!,
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
