f00ac1f67c1d5b2da8942f3e60aa6b93b8a4bae8
Rizki Alfarid <gusnafarid@gmail.com>
2024-06-05 00:05:44 +0700

    add get set by id endpoint

--- src/routes/exerciseSets.ts
+++ src/routes/exerciseSets.ts
@@ -27,4 +27,26 @@ router.get('/', verifyToken, async (req, res) => {
   }
 });
 
+router.get('/:setId', verifyToken, async (req, res) => {
+  const setId = req.params.setId;
+
+  try {
+    const result = await client.execute(`SELECT * FROM exercise_set WHERE set_id = ${setId}`);
+    if (result.rows.length <= 0) res.status(404).json('List not found');
+    const transformedRows = result.rows.map((row) => {
+      return {
+        id: row['set_id'],
+        weight: row['set_weight'],
+        repetition: row['set_repetition'],
+        dateCreated: row['set_date_created'],
+      };
+    });
+    res.status(200).json(transformedRows[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json('Error fetching set');
+  }
+});
+
+// TODO: create set endpoint
 export { router as exerciseSetsRouter };
